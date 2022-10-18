b5048d7548f73d6f347091326e529f917f2c8198
unknown <enockawor@gmail.com>
2022-10-17 22:19:31 -0700

    Reversed the changes in the members

--- database/migrations/2022_10_12_153825_create_members_table.php
+++ database/migrations/2022_10_12_153825_create_members_table.php
@@ -17,19 +17,6 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('other_names')->nullable();
-            $table->string('last_name');
-            $table->foreignIdFor(Member::class)->nullable();
-            $table->string('home_town')->nullable();
-            $table->text('nearest_landmark')->nullable();
-            $table->string('phone_number')->nullable();
-            $table->text('alt_phone_number')->nullable();
-            $table->string('status')->default('active');
-            $table->boolean('class_leader')->default(false);
-            $table->enum('leader_type', ['Asst.', 'Main'])->nullable();
-            $table->foreignIdFor(User::class);
-            $table->softDeletes();
             $table->timestamps();
         });
     }
