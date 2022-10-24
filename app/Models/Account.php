9d582882c8e131ceb3177e95f34f3d6163c3610b
Awortwe <enockawor@gmail.com>
2022-10-24 13:02:25 -0700

    Done with the account record

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
@@ -12,7 +13,17 @@ class Account extends Model
 
     protected $fillable = [
         'date',
-        'comments', 
+        'comments',
+        'account_record_id', 
         'user_id'
     ];
+
+    public function accountRecord(): BelongsTo
+    {
+        return $this->belongsTo(AccountRecord::class);
+    }
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
